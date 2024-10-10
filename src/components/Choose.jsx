bf43159e45361cdb3ead3c537a7b462675c78eba
zAHEEr <ujoshua976@gmail.com>
2024-10-10 08:39:03 +0100

    initial commit

---
+++ src/components/Choose.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import styled from "styled-components";
+import choose from "../assets/choose.png";
+import Button from "./Button";
+export default function Choose() {
+  return (
+    <Section>
+      <div className="image">
+        <img src={choose} alt="choose" />
+      </div>
+      <div className="content">
+        <h2>Why should you Choose Our NFTs ?</h2>
+        <p>
+          Our NFTs provides a unique way to establish and verify ownership of
+          digital assets. Each NFT is recorded on the blockchain, ensuring it is
+          one-of-a-kind and cannot be duplicated. This is particularly valuable
+          in the art world, where authenticity is crucial to prevent fraud and
+          forger
+        </p>
+        <Button text="Read More" blue />
+      </div>
+    </Section>
+  );
+}
+
+const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  margin: 0 9rem;
+  gap: 3rem;
+  margin-bottom: 5rem;
+  .image {
+  }
+  .content {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    gap: 3rem;
+    h2 {
+      font-size: 4rem;
+    }
+    p {
+      color: #7b7e86;
+      line-height: 2rem;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    flex-direction: column;
+    margin: 2rem;
+    .image {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img {
+        height: 50vw;
+      }
+    }
+    .content {
+      gap: 1rem;
+      h2 {
+        font-size: 1rem;
+      }
+      p {
+        line-height: 1.3rem;
+        font-size: 0.9rem;
+      }
+    }
+  }
+`;
